553876d34e0cf7be488a8a0912483f41de37b543
LucasNoliveira <lucasnoliveira126@gmail.com>
2026-02-10 16:59:33 -0300

    feat: add routes /dashboard and /login with basic protected route

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <>
+        <h1> Bem vindo(a) à página de Dashboard </h1>
+        </>
+    )
+}
+
+export default Dashboard;
---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <>
+        <h1> Bem vindo(a) à página de Login </h1>
+        </>
+    )
+}
+
+export default LoginPage;

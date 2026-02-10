3d6714f0f9b06529182e0b06143cc6ac1d3951d0
LucasNoliveira <lucasnoliveira126@gmail.com>
2026-02-10 17:21:55 -0300

    feat: set up Tailwind CSS

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 const LoginPage = () => {
     return (
         <>
-        <h1> Bem vindo(a) à página de Login </h1>
+        <h1 className="text-blue-700"> Bem vindo(a) à página de Login </h1>
         </>
     )
 }

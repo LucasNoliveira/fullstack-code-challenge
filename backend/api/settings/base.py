7dd1c3d1ff4840db99dacf9536b9fd8da5268c41
LucasNoliveira <lucasnoliveira126@gmail.com>
2026-02-11 14:05:12 -0300

    chore: update django INSTALLED_APPS

--- backend/api/settings/base.py
+++ backend/api/settings/base.py
@@ -16,6 +16,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'authentication',
+    'companies'
 ]
 
 MIDDLEWARE = [
